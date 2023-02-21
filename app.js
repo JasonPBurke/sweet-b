eec64afbae80df90459775667bf65a20678c97b6
Jason Burke <jpburke77@gmail.com>
2023-02-21 17:53:16 -0600

    added js touchstart for flip cards on ios

--- app.js
+++ app.js
@@ -5,23 +5,37 @@ let prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
   let currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("header1").style.top = "0";
+    document.getElementById('header1').style.top = '0';
   } else {
-    document.getElementById("header1").style.top = "-125px";
+    document.getElementById('header1').style.top = '-125px';
   }
   prevScrollpos = currentScrollPos;
 };
 
+//***** flip cards on touch ******/
+singleCardContainers = document.querySelectorAll('.single-card-container');
+for (let i of singleCardContainers) {
+  console.log(i);
+  i.addEventListener('touchstart', () => {
+    for (let j of singleCardContainers) {
+      j.childNodes[1].style.transform = 'none';
+    }
+    i.childNodes[1].style.transform = 'rotateY(180deg)';
+    console.log(i.childNodes[1]);
+    console.log('here');
+  });
+}
+
 //***** trigger map modal open and close ******/
 
-const modal = document.querySelector(".modal");
-const triggerArray = document.querySelectorAll(".modal-trigger");
-const closeButton = document.querySelector(".close-button");
-const mapContainer = document.querySelector(".modal-map-container");
-const currentMap = document.getElementById("map");
+const modal = document.querySelector('.modal');
+const triggerArray = document.querySelectorAll('.modal-trigger');
+const closeButton = document.querySelector('.close-button');
+const mapContainer = document.querySelector('.modal-map-container');
+const currentMap = document.getElementById('map');
 
 function toggleModal() {
-  modal.classList.toggle("show-modal");
+  modal.classList.toggle('show-modal');
 }
 
 function windowOnClick(event) {
@@ -33,35 +47,35 @@ function windowOnClick(event) {
 //******** load mapquest maps *********/
 
 function fetchMap(address) {
-  L.mapquest.key = "ck2OXUAJsF0iz999XGQ62jyXo8AXOVp7";
+  L.mapquest.key = 'ck2OXUAJsF0iz999XGQ62jyXo8AXOVp7';
 
   L.mapquest.geocoding().geocode(address, createMap);
 
   function createMap(error, response) {
     var location = response.results[0].locations[0];
     var latLng = location.displayLatLng;
-    var map = L.mapquest.map("map", {
+    var map = L.mapquest.map('map', {
       center: latLng,
-      layers: L.mapquest.tileLayer("map"),
+      layers: L.mapquest.tileLayer('map'),
       zoom: 12,
     });
 
     var customPopup = L.popup({ closeButton: false })
       .setLatLng(latLng)
-      .setContent("Event is located here.")
+      .setContent('Event is located here.')
       .openOn(map);
   }
 }
 
 function destroyMap() {
   map.remove();
-  let newMapDiv = document.createElement("div");
-  newMapDiv.setAttribute("id", "map");
+  let newMapDiv = document.createElement('div');
+  newMapDiv.setAttribute('id', 'map');
   mapContainer.appendChild(newMapDiv);
 }
 
 triggerArray.forEach((trigger) =>
-  trigger.addEventListener("click", function () {
+  trigger.addEventListener('click', function () {
     let address = trigger.childNodes[1].innerHTML;
     destroyMap();
     fetchMap(address);
@@ -69,5 +83,5 @@ triggerArray.forEach((trigger) =>
   })
 );
 
-closeButton.addEventListener("click", toggleModal);
-window.addEventListener("click", windowOnClick);
+closeButton.addEventListener('click', toggleModal);
+window.addEventListener('click', windowOnClick);
